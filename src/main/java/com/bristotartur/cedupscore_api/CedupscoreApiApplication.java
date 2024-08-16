e006941838cf4bec4c1d0c7b54a336e2d13b561d
bristotartur <arturdarosabristot@gmail.com>
2024-08-16 16:12:18 -0300

    Primeiro commit

---
+++ src/main/java/com/bristotartur/cedupscore_api/CedupscoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.bristotartur.cedupscore_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CedupscoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CedupscoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bristotartur/cedupscore_api/CedupscoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bristotartur.cedupscore_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CedupscoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
