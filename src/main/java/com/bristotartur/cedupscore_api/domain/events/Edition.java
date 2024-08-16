7a5ef5f01fcf2167c069f84ec7c0a8616d2ac34d
bristotartur <arturdarosabristot@gmail.com>
2024-08-16 19:43:41 -0300

    Entidade Edition adicionada

---
+++ src/main/java/com/bristotartur/cedupscore_api/domain/events/Edition.java
@@ -0,0 +1,56 @@
+package com.bristotartur.cedupscore_api.domain.events;
+
+import com.bristotartur.cedupscore_api.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "TB_EDITION")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Edition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate closingDate;
+
+    @Override
+    public String toString() {
+        return "Edition{" +
+                "id=" + id +
+                ", status=" + status +
+                ", startDate=" + startDate +
+                ", closingDate=" + closingDate +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Edition edition = (Edition) o;
+        return Objects.equals(id, edition.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
---
+++ src/main/java/com/bristotartur/cedupscore_api/enums/Status.java
@@ -0,0 +1,10 @@
+package com.bristotartur.cedupscore_api.enums;
+
+public enum Status {
+    SCHEDULED,
+    IN_PROGRESS,
+    STOPPED,
+    ENDED,
+    CANCELED,
+    OPEN_FOR_EDITS
+}
