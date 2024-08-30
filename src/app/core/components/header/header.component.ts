0ea1436e3b47579c31ff1841ecdeae74fca480a1
bristotartur <arturdarosabristot@gmail.com>
2024-08-30 15:23:39 -0300

    Componentes header e nav-item adicionados

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { NavItemComponent } from '../nav-item/nav-item.component';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    NavItemComponent
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent implements OnInit {
+
+  title = 'Cedupscore';
+  selectedItem!: number;
+
+  navItems = [
+    { name: 'Geral', iconClass: 'fa-solid fa-house', link: '', isSelected: true },
+    { name: 'Placar', iconClass: 'fa-solid fa-medal', link: '', isSelected: false },
+    { name: 'Tarefas', iconClass: 'fa-solid fa-list-check', link: '', isSelected: false },
+    { name: 'Esportes', iconClass: 'fa-solid fa-volleyball', link: '', isSelected: false },
+    { name: 'Membros', iconClass: 'fa-solid fa-person', link: '', isSelected: false },
+    { name: 'Recursos', iconClass: 'fa-solid fa-exclamation', link: '', isSelected: false }
+  ];
+
+  constructor (private router: Router) {  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.updateSelectedItem(this.router.url);
+    });
+  }
+
+  selectItem(index: number): void {
+    this.navItems.forEach((item, i) => {
+      item.isSelected = (i == index);
+    });
+  }
+
+  updateSelectedItem(url: string): void {
+    const index = this.navItems
+      .findIndex(item => item.link == url);
+
+    this.selectItem(index != -1 ? index : 0);
+  }
+
+}
